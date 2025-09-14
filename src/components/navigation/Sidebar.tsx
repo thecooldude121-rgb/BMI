fd855565fc7009bddb6e24a7875fb38aca562d31
Venkatraj <thecooldude121@gmail.com>
2025-09-14 22:22:08 +0530

    Updated App.tsx

--- src/components/navigation/Sidebar.tsx
+++ src/components/navigation/Sidebar.tsx
@@ -3,7 +3,7 @@ import { NavLink, useLocation } from 'react-router-dom';
 import { 
   LayoutDashboard, Users, DollarSign, Target, BarChart3, Calendar, 
   Settings, UserCheck, Building2, Phone, Mail, Activity, CheckSquare,
-  TrendingUp, Zap, Award, Trophy
+  TrendingUp, Zap, Award
 } from 'lucide-react';
 import { useAuth } from '../../contexts/AuthContext';
 
